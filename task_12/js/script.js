6e1f38b29e53c0de22c869a99735736ac23f8d6d
SerafimPoch <serpochapinsky@gmail.com>
2018-01-30 23:22:58 +0200

    add clock

--- task_12/js/script.js
+++ task_12/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello Serafim')
+const clock = () => {
+    let time = new Date(),
+        hours = time.getHours(),
+        minutes = time.getMinutes(),
+        seconds = time.getSeconds();
+
+const harold = (standIn) => {
+    return standIn < 10 ? 
+                standIn = '0' + standIn
+                        :
+                standIn
+          }
+document.querySelector('#time').innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+    
+}
+setInterval(clock, 1000);
